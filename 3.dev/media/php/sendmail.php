061077e8566aef9208929ddd3781866a3ba369a8
Phil Huffstickler <phil@honemedia.com>
2012-06-18 17:13:53 -0400

    More UI tweaks.

---
+++ 3.dev/media/php/sendmail.php
@@ -0,0 +1,18 @@
+<?php
+    $mail=$_POST['contactEmail'];
+    $name=$_POST['contactName'];
+    $subject='HoneMedia.com - Inquiry';
+    $text=$_POST['contactMessage'];
+
+    $to='phil@honemedia.com';
+    $message='Client: '.$mail;
+    $message.=$text;
+
+    if(mail($to, $subject,$message)){
+    echo 'mail successful send';
+    }
+    else{
+    echo 'there’s some errors to send the mail, verify your server options';
+    }
+?>
+
